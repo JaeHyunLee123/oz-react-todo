3b79bf098feb76597d19179117839859c4b7555b
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-05-29 13:03:51 +0900

    feat: todo check isComplete

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -12,13 +12,28 @@ import "./Todo.css";
  * @param {TodoProps} props
  */
 export default function Todo({ todo, setTodos }) {
+  const onCompleteButtonClick = () => {
+    /**@type {Todo} */
+    const newTodo = {
+      id: todo.id,
+      todo: todo.todo,
+      isComplete: !todo.isComplete,
+    };
+
+    setTodos((todos) =>
+      todos.map((element) => (todo.id === element.id ? newTodo : element))
+    );
+  };
+
   const onDeleteButtonClick = () => {
     setTodos((todos) => todos.filter((element) => todo.id !== element.id));
   };
 
   return (
     <div className="todo-box">
+      <button onClick={onCompleteButtonClick}>✅</button>
       <span>{todo.todo}</span>
+      {todo.isComplete ? "end" : ""}
       <button onClick={onDeleteButtonClick}>❌</button>
     </div>
   );
