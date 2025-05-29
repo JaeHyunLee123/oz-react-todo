7abe3e45252b7c9cef0f2da2d6353adb85cfe1c3
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-05-29 11:01:28 +0900

    feat: random background

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import backgroundImage from "./libs/background";
+import "./App.css";
+
 function App() {
-  return <></>;
+  return (
+    <div
+      className="body"
+      // 랜덤 배경 때문에 어쩔 수 없이 인라인 속성 사용
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh", // full viewport height
+        width: "100vw", // full viewport width
+      }}
+    >
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
